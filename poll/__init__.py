0d60ab70776a00920cb153924bc14bbf4bf2aac5
JellyFrost6 <99845342+JellyFrost6@users.noreply.github.com>
2022-02-21 12:42:08 +0100

    Update __init__.py

--- poll/__init__.py
+++ poll/__init__.py
@@ -1,6 +1,6 @@
 from redbot.core.bot import Red
 
-from .poll import Poll
+from .poll import poll
 
 __red_end_user_data_statement__ = (
     "This cog does not persistently store data or metadata about users."
@@ -8,5 +8,5 @@ __red_end_user_data_statement__ = (
 
 
 def setup(bot: Red):
-    cog = Poll(bot)
+    cog = poll(bot)
     bot.add_cog(cog)
